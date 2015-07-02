42d214c2b74ce7133f91520f6114cb7cadabc982
Ilias Tsangaris <iliastsangaris@gmail.com>
2015-07-01 19:58:37 -0700

    find by ndex

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -45,7 +45,7 @@ class PokemonsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pokemon
-      @pokemon = Pokemon.find(params[:id])
+      @pokemon = Pokemon.find_by(ndex: params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
